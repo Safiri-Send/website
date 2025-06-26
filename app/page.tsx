7b686c393dcd1f8f3cc479fa543fa222e631364c
JamesVictor-o <victorjames40v@gmail.com>
2025-06-27 00:18:19 +0100

    feat: implement the call to action section

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CTASection from './components/Cta-section'
+
+const page = () => {
+  return (
+    <div className='px-5'>
+      <CTASection/>
+    </div>
+  )
+}
+
+export default page
