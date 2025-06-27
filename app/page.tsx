bbe2e73c49a8f0f005f9c3a5d65a64b23a861682
lopwusjethro92@gmail.com <lopwusjethro92@gmail.com>
2025-06-27 17:27:31 +0100

    feat: implement device compatibility component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DeviceCompatibility from "@/app/components/DeviceCompatibility";
+
+const page = () => {
+  return (
+    <div>
+      <DeviceCompatibility />
+    </div>
+  );
+};
+
+export default page;
